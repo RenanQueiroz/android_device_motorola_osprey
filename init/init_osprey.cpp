428dd4f4568ef6fccc93c4c9a0317e53b5baec7a
Kyle Harrison <khwebmail@gmail.com>
2018-09-04 10:45:10 +0100

    osprey: Load dummy initial BT MAC address.
    The real one will be loaded by hci_qcomm_init, but the BT HAL wants an
    address to be set before that point.
    
    Change-Id: Ia8ff499ccaa862ad697dcfecb969b29f29ee89f4

--- init/init_osprey.cpp
+++ init/init_osprey.cpp
@@ -188,4 +188,7 @@ void vendor_load_properties()
     property_override_dual("ro.build.description", "ro.vendor.build.description", description);
     property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", fingerprint);
     property_set("ro.mot.build.customerid", customerid);
+
+    // Init a dummy BT MAC address, will be overwritten later
+    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
 }
